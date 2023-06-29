55105e977c90fe7e2cc5c6bc0888ccd9a0f6efd2
Abhishek Nair <abhisheknair1441@gmail.com>
2023-06-29 13:56:01 +0530

    add login feature

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,16 @@
 import userModel from "../modals/user.modal.js";
 import UserModel from "../modals/user.modal.js";
-
+import bcrypt from "bcrypt";
 //get all users
 export const getAllUsers = async (req, res) => {
   try {
     const users = await userModel.find({});
-    return res
-      .status(200)
-      .json({ userCount:users.length,success: true, message: "all user data", users });
+    return res.status(200).json({
+      userCount: users.length,
+      success: true,
+      message: "all user data",
+      users,
+    });
   } catch (error) {
     console.log(error);
     return res
@@ -34,10 +37,10 @@ export const createUser = async (req, res) => {
         success: false,
         message: "user already exists with given email",
       });
-
-      //save new user
     }
-    const user = new UserModel({ username, email, password });
+    //save new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new UserModel({ username, email, password: hashedPassword });
     await user.save();
     return res
       .status(201)
@@ -54,4 +57,39 @@ export const createUser = async (req, res) => {
 
 //login
 
-export const loginUser = () => {};
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(401)
+        .json({ message: "Fill the required fields", success: false });
+    }
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "user not found with given email id",
+        success: false,
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "invalid username or password",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "login successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Error in login callback",
+      success: false,
+      error,
+    });
+  }
+};
